ed51007173231164bd54e0ebf50d78ed264f8767
lboumahdi <linaboumahdi1@gmail.com>
2024-04-24 14:17:53 +0200

    j

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:36:36 by lboumahd          #+#    #+#             */
-/*   Updated: 2024/04/22 18:09:30 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/04/24 13:54:35 by lboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	a = dst;
 	b = src;
+	if (!dst && !src)
+		return (0);
 	while (0 < n)
 	{
 		*(a++) = *(b++);
